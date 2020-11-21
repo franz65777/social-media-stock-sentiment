8dec2bbd420e2e55bce8396cee6d4ecb733b28e0
JGolafshan <jgolafshan@outlook.com>
2020-11-22 07:36:33 +1000

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WSB-Sentiment
+ analyze wallstreetbets sentime
---
+++ tests.py
@@ -0,0 +1,6 @@
+import app.WSBAuthentication as creds
+import app.WSBSentiment as wsb
+import matplotlib as plot
+
+sentiment = wsb.WallStreetBetsSentiment(autho_dict=creds.autho, posts=1)
+print(sentiment.parser(enable_debug=False)) # Use enable_debug=True to save the log of comments. (slows down performance)

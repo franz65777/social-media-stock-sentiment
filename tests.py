f8f21c104ffb353cdc8a9f4780b0b660230a3df1
JGolafshan <jgolafshan@outlook.com>
2020-11-28 21:00:39 +1000

    renamed app_3.py

--- tests.py
+++ tests.py
@@ -1,9 +1,9 @@
 import app.WSBAuthentication as creds
-import app.app_3 as WSB
+import app.WSBPrimary as WSB
 from pprint import pprint
 import os
 import pandas as pd
 
 instance = WSB.WallStreetBets(autho_dict=creds.autho, posts=1)
 data = instance.read_data
-instance.debug(data=data)
+print(data)

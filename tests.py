c5170f4bd5d51457cb9ee64dc8f716e667401b0c
JGolafshan <jgolafshan@outlook.com>
2020-11-28 14:19:56 +1000

    test

--- tests.py
+++ tests.py
@@ -1,9 +1,7 @@
 import app.WSBAuthentication as creds
-import app_2.WSBTicker as ticker
+import app_3.app_3 as WSB
 from pprint import pprint
 
+instance = WSB.WallStreetBets(autho_dict=creds.autho, posts=1)
 
-
-
-tickers_data = ticker.Ticker(autho_dict=creds.autho, posts=1)
-pprint(tickers_data.create_tickers())
+print(instance.break_up_data)

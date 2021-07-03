2e8d72e106a0ebaa731d918aa02ce11eb587d367
Joshua Golafshan <JGolafshan@outlook.com>
2021-07-04 07:42:57 +1000

    Update base_tests.py

--- tests/base_tests.py
+++ tests/base_tests.py
@@ -2,18 +2,18 @@ from threading import Thread
 import app.data_gathering as data
 
 reddit_cred = {
-    'app_id': 'AuUIHLYhwPDurw',
-    'secret': 'r-h6vCcjnhl8WLMyWJuDaxhh2MuM2A',
-    'username': 'wsbtestbot69420',
-    'password': 'qgVi7zceCJwz2B9',
-    'user_agent': "WSB Sentiment Test Bot"
+    'app_id': '',
+    'secret': '',
+    'username': '',
+    'password': '',
+    'user_agent': ''
 }
 
 twitter_cred = {
-    'consumer_key': 'zKqMuzEf33DfiqYBXJcnDItVz',
-    'consumer_secret': '1rytFyA9c16em7IylAyzdkoiIhgGRBX9DiybY5txUnXwrYHXH9',
-    'access_token_key': '3295534592-UN6oheD2ybt1ZTRb794iWw91yIxGC3w9p46fPFM',
-    'access_token_secret': 'tLs94SB1hEPqighqDzAYR9zBBO4TvN7XlmDuTDK46DN26'
+    'consumer_key': '',
+    'consumer_secret': '',
+    'access_token_key': '',
+    'access_token_secret': ''
 }
 
 reddit = data.Reddit(client_id=reddit_cred.get('app_id'),
